d9ea03eb37ce17a180a8f04e3224c8775191b279
Roman Vladimirov <trueromanus@gmail.com>
2026-01-04 20:54:47 +0300

    Remove inheritance for ReleaseTorrentSaveModel

--- src/SaveModels/ReleaseTorrentSaveModel.cs
+++ src/SaveModels/ReleaseTorrentSaveModel.cs
@@ -1,16 +1,32 @@
 ﻿using LocalCacheChecker.ApiModels;
-using System.Text.Json.Serialization;
 
 namespace LocalCacheChecker.SaveModels {
 
-    internal record ReleaseTorrentSaveModel : ReleaseTorrentModel {
+    internal record ReleaseTorrentSaveModel {
 
         public int ReleaseId { get; set; }
 
         public long Time { get; set; }
 
-        [JsonIgnore]
-        public new string UpdatedAt { get; init; } = "";
+        public string Hash { get; init; } = "";
+
+        public int Id { get; init; }
+
+        public string Magnet { get; init; } = "";
+
+        public string Filename { get; init; } = "";
+
+        public string Description { get; init; } = "";
+
+        public StringValueItem Quality { get; init; } = new StringValueItem ();
+
+        public StringValueItem Codec { get; init; } = new StringValueItem ();
+
+        public StringValueItem Type { get; init; } = new StringValueItem ();
+
+        public long Size { get; init; }
+
+        public int Seeders { get; init; }
 
     }
 
